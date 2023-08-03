f3a664a13c959abfad9f3298db65b615b6f69e7e
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-08-03 18:59:34 +0530

    solving back-pressure problem using pipe operator

--- how-node-works/streams.js
+++ how-node-works/streams.js
@@ -8,21 +8,26 @@ server.on("request", (req, res) => {
   //   res.end(data);
   // });
 
-  // Solution 2
+  // Solution 2: streams
+  // const readable = fs.createReadStream("test-file.txt");
+  // readable.on("data", (chunk) => {
+  //   res.write(chunk);
+  // });
+
+  // readable.on("end", () => {
+  //   res.end();
+  // });
+
+  // readable.on("error", (err) => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end("File not found!");
+  // });
+
+  // Solution 3
   const readable = fs.createReadStream("test-file.txt");
-  readable.on("data", (chunk) => {
-    res.write(chunk);
-  });
-
-  readable.on("end", () => {
-    res.end();
-  });
-
-  readable.on("error", (err) => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end("File not found!");
-  });
+  readable.pipe(res);
+  // readableSource.pipe(writableDestination)
 });
 
 server.listen(8000, "127.0.0.1", () => {

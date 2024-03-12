a34d70d9bbe52a77b17a9cf8dd932e9d326bfa2b
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2024-03-12 19:13:27 +0530

    feat: patch tours api

--- natours/app.js
+++ natours/app.js
@@ -61,6 +61,23 @@ app.get("/api/v1/tours/:id", (req, res) => {
   });
 });
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { tour: "<Updated tour will come here...>" },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);

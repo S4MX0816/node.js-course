5dfd3e290fe84661ae4c2db418b15075a25168ff
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2024-03-13 18:13:51 +0530

    feat: third party middleware morgan

--- natours/app.js
+++ natours/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,6 +22,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
   // JSend format
   res.status(200).json({
@@ -102,6 +107,7 @@ const deleteTour = (req, res) => {
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
+// 3) ROUTES
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 
 app
@@ -110,6 +116,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);

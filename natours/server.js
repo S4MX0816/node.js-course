a7b995760e6443d7ff7ac486731275c773d00e1c
sanyam trehan <trehansanyam@gmail.com>
2024-07-12 20:07:34 +0530

    feat(natours): mongoose and db connection added

--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,18 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Connection Successful!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

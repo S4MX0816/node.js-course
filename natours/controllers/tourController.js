f739ced143391a75a253b8aabae3c4f1be5f902f
sanyam trehan <trehansanyam@gmail.com>
2024-07-13 21:16:10 +0530

    refactor(natours): mvc architecture for tours

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,22 +1,4 @@
-const fs = require("fs");
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  val = val * 1;
-  const tour = tours.find((el) => el.id === val);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  next();
-};
+const Tour = require("../models/tourModel");
 
 exports.checkBody = (req, res, next) => {
   const { name, price } = req.body;
@@ -34,36 +16,25 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     requestedAt: req.requestTime,
-    result: tours.length,
-    data: { tours },
+    // result: tours.length,
+    // data: { tours },
   });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: { tour: newTour },
-      });
-    }
-  );
+  res.status(201).json({
+    status: "success",
+    // data: { tour: newTour },
+  });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: "success",
-    data: { tour },
-  });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: "success",
+  //   data: { tour },
+  // });
 };
 
 exports.updateTour = (req, res) => {
